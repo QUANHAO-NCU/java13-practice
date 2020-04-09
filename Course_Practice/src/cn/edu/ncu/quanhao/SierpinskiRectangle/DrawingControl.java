06ab73cc258387b833308e40c2ba28f45b3d7a8c
QUANHAO-NCU <44420952+QUANHAO-NCU@users.noreply.github.com>
2020-04-09 21:33:39 +0800

    done SierpinskiRectangle!

--- Course_Practice/src/cn/edu/ncu/quanhao/SierpinskiRectangle/DrawingControl.java
+++ Course_Practice/src/cn/edu/ncu/quanhao/SierpinskiRectangle/DrawingControl.java
@@ -1,4 +1,75 @@
 package cn.edu.ncu.quanhao.SierpinskiRectangle;
 
-public class DrawingControl {
+import cn.edu.ncu.quanhao.Sierpinski.DrawTriangle;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
+
+import java.awt.*;
+import java.util.Stack;
+
+/**
+ * 控制模型C
+ * 塞尔平斯基矩形最终在这里控制绘画
+ */
+public class DrawingControl extends Pane {
+    public Canvas canvas;
+    public GraphicsContext gc;
+    private MyRectangle myRectangle;
+    private DrawRectangle dr = new DrawRectangle();
+    public double level;
+    private double MaxLen;
+
+    DrawingControl(double level, double MaxLen) {
+        this.level = level;
+        this.MaxLen = MaxLen;
+        canvas = new Canvas(MaxLen + 50 * 2, MaxLen + 50 * 2);
+        this.gc = canvas.getGraphicsContext2D();
+        generate(level, MaxLen);
+        getChildren().add(canvas);
+    }
+
+    private void generate(double level, double maxLen) {
+        System.out.println("Start Drawing!");
+        Stack<MyRectangle> stack = new Stack<>();
+        myRectangle = new MyRectangle(50, 50, this.MaxLen, this.MaxLen, 0, Color.WHITE);
+        stack.push(myRectangle);
+        double l = 0;
+        do {
+            myRectangle = stack.pop();
+            l = myRectangle.getLevel();
+            l++;
+            MyRectangle RUL = new MyRectangle(myRectangle.getUpLeftREC(), l,Color.RED);
+            MyRectangle RUC = new MyRectangle(myRectangle.getUpCenterREC(), l,Color.ORANGE);
+            MyRectangle RUR = new MyRectangle(myRectangle.getUpRightREC(), l,Color.YELLOW);
+            MyRectangle RCL = new MyRectangle(myRectangle.getCenterLeftREC(), l,Color.GREEN);
+            MyRectangle RCR = new MyRectangle(myRectangle.getCenterRightREC(),l, Color.BLUE);
+            MyRectangle RDL = new MyRectangle(myRectangle.getDownLeftREC(), l,Color.BLUE);
+            MyRectangle RDC = new MyRectangle(myRectangle.getDownCenterREC(), l,Color.PURPLE);
+            MyRectangle RDR = new MyRectangle(myRectangle.getDownRightREC(), l,Color.GOLD);
+
+            dr.drawRectangle(this.gc, myRectangle);
+            dr.drawRectangle(this.gc, RUL);
+            dr.drawRectangle(this.gc, RUC);
+            dr.drawRectangle(this.gc, RUR);
+            dr.drawRectangle(this.gc, RCL);
+            dr.drawRectangle(this.gc, RCR);
+            dr.drawRectangle(this.gc, RDL);
+            dr.drawRectangle(this.gc, RDC);
+            dr.drawRectangle(this.gc, RDR);
+            dr.drawEmpty(this.gc,myRectangle);
+            if (l < this.level) {
+                stack.push(RUL);
+                stack.push(RUC);
+                stack.push(RUR);
+                stack.push(RCL);
+                stack.push(RCR);
+                stack.push(RDL);
+                stack.push(RDC);
+                stack.push(RDR);
+            }
+        } while (!stack.isEmpty());
+        System.out.println("Drawing done!");
+    }
 }
--- Course_Practice/src/cn/edu/ncu/quanhao/SierpinskiRectangle/MyRectangle.java
+++ Course_Practice/src/cn/edu/ncu/quanhao/SierpinskiRectangle/MyRectangle.java
@@ -1,23 +1,31 @@
 package cn.edu.ncu.quanhao.SierpinskiRectangle;
 
+import javafx.scene.paint.Color;
+
 public class MyRectangle {
-    double X, Y, width, height;
+    double X, Y, width, height, level;
     double SubUpLeftX, SubUpCenterX, SubUpRightX, SubCenterLeftX, SubCenterRightX, SubDownLeftX, SubDownCenterX, SubDownRightX;
     double SubUpLeftY, SubUpCenterY, SubUpRightY, SubCenterLeftY, SubCenterRightY, SubDownLeftY, SubDownCenterY, SubDownRightY;
-    double SubWidth,SubHeight;
-    MyRectangle(double X, double Y, double width, double height) {
+    double SubWidth, SubHeight;
+    Color color;
+
+    MyRectangle(double X, double Y, double width, double height, double level, Color color) {
         this.X = X;
         this.Y = Y;
         this.width = width;
         this.height = height;
+        this.level = level;
+        this.color = color;
         CalParameters();//计算子矩形的坐标参数
     }
 
-    MyRectangle(double[] points, double width, double height) {
-        this.X = points[0];
-        this.Y = points[1];
-        this.width = width;
-        this.height = height;
+    MyRectangle(double[] data, double level,Color color) {
+        this.X = data[0];
+        this.Y = data[1];
+        this.width = data[2];
+        this.height = data[3];
+        this.level = level;
+        this.color = color;
         CalParameters();//计算子矩形的坐标参数
     }
 
@@ -47,7 +55,60 @@ public class MyRectangle {
         this.SubDownRightX = this.X + this.width / 3 * 2;
         this.SubDownRightY = this.Y + this.height / 3 * 2;
         //
-        this.SubWidth = this.width/3;
-        this.SubHeight = this.height/3;
+        this.SubWidth = this.width / 3;
+        this.SubHeight = this.height / 3;
     }
+
+    public double[] getUpRightREC() {
+        double[] data = {this.SubUpRightX, this.SubUpRightY, this.SubWidth, this.SubHeight, this.level};
+        return data;
+    }
+
+    public double[] getUpCenterREC() {
+        double[] data = {this.SubUpCenterX, this.SubUpCenterY, this.SubWidth, this.SubHeight, this.level};
+        return data;
+    }
+
+    public double[] getUpLeftREC() {
+        double[] data = {this.SubUpLeftX, this.SubUpLeftY, this.SubWidth, this.SubHeight, this.level};
+        return data;
+    }
+
+    public double[] getCenterRightREC() {
+        double[] data = {this.SubCenterRightX, this.SubCenterLeftY, this.SubWidth, this.SubHeight, this.level};
+        return data;
+    }
+
+    public double[] getCenterLeftREC() {
+        double[] data = {this.SubCenterLeftX, this.SubCenterLeftY, this.SubWidth, this.SubHeight, this.level};
+        return data;
+    }
+
+    public double[] getDownRightREC() {
+        double[] data = {this.SubDownRightX, this.SubDownRightY, this.SubWidth, this.SubHeight, this.level};
+        return data;
+    }
+
+    public double[] getDownCenterREC() {
+        double[] data = {this.SubDownCenterX, this.SubDownCenterY, this.SubWidth, this.SubHeight, this.level};
+        return data;
+    }
+
+    public double[] getDownLeftREC() {
+        double[] data = {this.SubDownLeftX, this.SubDownLeftY, this.SubWidth, this.SubHeight, this.level};
+        return data;
+    }
+
+    public double getEmptyRX() {
+        return this.X + this.SubWidth;
+    }
+
+    public double getEmptyRY() {
+        return this.Y + this.SubHeight;
+    }
+
+    public double getLevel() {
+        return this.level;
+    }
+
 }
